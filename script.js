9adac53ab2ca2c2786813eb33540a4beb37c366d
Qj Stout <qjstoutspykers@gmail.com>
2021-07-31 00:50:32 +1000

    Add pixel 'drawing' capability

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const gridHeight = 16;
-const gridWidth = 16;
+const GRID_HEIGHT = 16;
+const GRID_WIDTH = 16;
 
-const gridContainer = document.getElementById('grid');
-getNewGrid(gridWidth, gridHeight).forEach(row => {
-    gridContainer.appendChild(row);
-});
+const PIXEL_CLASS = "pixel";
+const PIXEL_DRAWN_CLASS = "drawn";
+const ROW_CLASS = "row";
+const GRID_CLASS = "grid";
 
-function getNewPixel() {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    return pixel;
+const gridContainer = document.getElementById(GRID_CLASS);
+getNewGrid(GRID_WIDTH, GRID_HEIGHT).forEach(appendRowToGrid, gridContainer);
+addMouseOverToPixels();
+
+function addMouseOverToPixels() {
+    const pixels = document.querySelectorAll(`.${PIXEL_CLASS}`);
+
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseenter', mouseOverEvent);
+    });
 }
 
-function getNewRow(rowLength) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+function mouseOverEvent() {
+    this.removeEventListener('mouseenter', mouseOverEvent);
+    this.classList.add(PIXEL_DRAWN_CLASS);
+}
 
-    for (let i = 0; i < rowLength; i++) {
-        row.appendChild(getNewPixel());
-    }
-    
-    return row;
+function appendRowToGrid(row) {
+    this.appendChild(row);
 }
 
 function getNewGrid(width, height) {
@@ -31,4 +35,21 @@ function getNewGrid(width, height) {
     }
 
     return grid;
+}
+
+function getNewRow(rowLength) {
+    const row = document.createElement('div');
+    row.classList.add(ROW_CLASS);
+
+    for (let i = 0; i < rowLength; i++) {
+        row.appendChild(getNewPixel());
+    }
+    
+    return row;
+}
+
+function getNewPixel() {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    return pixel;
 }
